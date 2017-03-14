cac8af819f07fbad6dfb7532c71e48cc50935751
lixingxing <lixx@vsettan.com.cn>
2017-03-14 16:31:40 +0800

    update config haproxy shell

--- haproxy/config.sh
+++ haproxy/config.sh
@@ -4,6 +4,10 @@ CURDIR=$(cd $(dirname "$0") && pwd)
 
 test -f /etc/haproxy/haproxy.cfg && mv /etc/haproxy/haproxy.cfg{,.bk}
 cp ${CURDIR}/haproxy.cfg /etc/haproxy/
+
+echo net.ipv4.ip_nonlocal_bind=1 >/etc/sysctl.d/haproxy.conf
 echo 1 > /proc/sys/net/ipv4/ip_nonlocal_bind
-cp ${CURDIR}/haproxy-rsyslog.conf /etc/sysctl.d/
 sysctl -p
+
+cp ${CURDIR}/haproxy-rsyslog.conf /etc/rsyslog.d/haproxy-rsyslog.conf
+systemctl restart rsyslog
