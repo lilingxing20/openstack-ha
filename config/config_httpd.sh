a1ac49ba780aaab75376ccde27a6187303cf70b8
lixingxing <lixx@vsettan.com.cn>
2017-03-15 10:43:36 +0800

    update config horizon

--- config/config_httpd.sh
+++ config/config_httpd.sh
@@ -11,3 +11,5 @@ sed -i "s/Listen 35357$/Listen $(hostname -s):35357/" /etc/httpd/conf/ports.conf
 sed -i "s/Listen 5000$/Listen $(hostname -s):5000/" /etc/httpd/conf/ports.conf
 #sed -i "s/Listen 8041$/Listen $(hostname -s):8041/" /etc/httpd/conf/ports.conf
 #sed -i "s/Listen 8042$/Listen $(hostname -s):8042/" /etc/httpd/conf/ports.conf
+
+sed -i 's/^OPENSTACK_KEYSTONE_URL.*/OPENSTACK_KEYSTONE_URL = "http:\/\/controller_vip:5000\/v2.0"/' /usr/share/openstack-dashboard/openstack_dashboard/local/local_settings.py
